3da885051e9b460b7dd7c01698f22a805b5fc694
Jorge Guzman <jorgeguzman@Jorges-MacBook-Air.local>
2024-07-20 11:33:48 -0400

    added handlebars

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,3 +1,7 @@
-const router = require('express').Router();
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+  res.render("all");
+});
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,21 @@
+//Dependencies
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
+const hbs = exphbs.create({});
 
 //created new sequelize store using the express-session package
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+//set up express app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+
 
 //configure and link a session object with the sequelize store
 
