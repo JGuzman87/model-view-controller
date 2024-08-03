d30f3bf2edd2d3f2912b6591b0e4267ed9484fcc
JGuzman87 <jguzman913@gmail.com>
2024-08-02 22:35:07 -0400

    started userRoutes

---
+++ controllers/UserRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { User } = require('../models');
+
+//if a POST request is made to /api/users, a new user is created.
+router.post('/', async (req, res) => {
+    try{
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            req.statusCode(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
---
+++ public/js/login.js
@@ -0,0 +1,9 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+}
+
+//collect the values from the login form
+const email = document.querySelector('#email-login').value.trim();
+const password = document.querySelector('#password-login').value.trim();
+
+    
