3061bc3ae605ee43886261315ec11c1066300dc9
JGuzman87 <jguzman913@gmail.com>
2024-09-05 23:09:15 -0400

    sequelize error fix

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,26 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const sequelize = process.env.DB_URL
-? new Sequelize(process.env.DB_URL)
-: new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'postgres',
-    }
-);
+// Create the sequelize connection instance
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // This is important for self-signed certificates on services like Render
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME, // For local development
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST || "localhost", // Use localhost or DB_HOST
+        dialect: "postgres",
+      }
+    );
 
 module.exports = sequelize;
